675a845498ed21eb233e454c2fd1846427154c9a
shershen-228 <mr.kolyamba.2000@mail.ru>
2021-11-18 17:00:41 +0300

    add function MyPow()

--- Funcs.h
+++ Funcs.h
@@ -2,8 +2,9 @@
 #ifndef __FUNCS_H__
 #define __FUNCS_H__
 double MySum(double, double);
-double MySub(double, double);
+double MySub(
+double, double);
 double MyMul(double, double);
 double MyDiv(double, double);
-double MyTest(double, double);
+int MyPow(int, int);
 #endif // __FUNCS_H__
